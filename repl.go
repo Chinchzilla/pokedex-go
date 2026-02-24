612687f3e3df3ce9f9af8893a80a4d11ee7a98b6
Chinchzilla <git@chinchzilla.slmail.me>
2026-02-24 12:42:15 +0100

    fixup! update repl logic

--- repl.go
+++ repl.go
@@ -42,7 +42,8 @@ func commandExit() error {
 
 func commandHelp() error {
 	cliCommands := getCommands()
-	fmt.Println("Available commands:")
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
 	for _, command := range cliCommands {
 		fmt.Printf("%s - %s\n", command.name, command.description)
 	}
